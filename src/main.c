6713127d4b16bbff17a860864e67dbccf3bf8f7f
Philip Lundrigan <philiplundrigan@gmail.com>
2020-10-28 08:00:37 -0600

    Add more stub code

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -5,4 +6,29 @@
 #include "http_server.h"
 #include "log.h"
 
-int main(int argc, char *argv[]) {}
+pthread_t *add_thread(pthread_t *threads[], int *thread_count, int *thread_size) {
+    // If we don't have room, add more space in the threads array
+    if (*thread_count == *thread_size) {
+        *thread_size *= 2; // Double it
+
+        *threads = realloc(*threads, sizeof(**threads) * (*thread_size));
+    }
+
+    pthread_t *new_thread = &(*threads)[*thread_count];
+    (*thread_count)++;
+    return new_thread;
+}
+
+void *handle_client(void *arg) {}
+
+int main(int argc, char *argv[]) {
+    int thread_count = 0;
+    int thread_size = 5;
+    pthread_t *threads = malloc(sizeof *threads * thread_size);
+
+    ///// Do server work /////
+
+    free(threads);
+
+    return EXIT_SUCCESS;
+}
